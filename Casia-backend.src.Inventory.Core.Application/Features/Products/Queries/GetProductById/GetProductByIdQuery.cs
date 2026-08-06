ae75f2d67a785716bdf513251a8a1824b82768d4
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2026-08-06 18:59:35 +0200

    Added two Query Handlers

---
+++ Casia-backend.src.Inventory.Core.Application/Features/Products/Queries/GetProductById/GetProductByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Casia_backend.src.Shared.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Casia_backend.src.Inventory.Core.Application.Features.Products.Queries.GetProductsById
+{
+    public sealed record GetProductByIdQuery(Guid Id) : IQuery;
+}
---
+++ Casia-backend.src.Inventory.Core.Application/Features/Products/Queries/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Casia_backend.src.Inventory.Core.Application.Features.Products.Queries.GetProductById
+{
+    public sealed class GetProductByIdQueryHandler
+    {
+    }
+}
