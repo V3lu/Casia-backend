88eced29cd2aa813aed048c10f0d66d2a28e5ab4
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2026-08-08 14:39:09 +0200

    Skeleton of another command handler

--- Casia-backend.src.Inventory.Core.Application/Features/Categories/Commands/AddCategory/AddCategoryCommandHandler.cs
+++ Casia-backend.src.Inventory.Core.Application/Features/Categories/Commands/AddCategory/AddCategoryCommandHandler.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Casia_backend.src.Inventory.Core.Domain.DTOs;
+using Casia_backend.src.Inventory.Core.Domain.Repositories;
+using Casia_backend.src.Shared.Commands;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Casia_backend.src.Inventory.Core.Application.Features.Categories.Commands.AddCategory
 {
-    public sealed class AddCategoryCommandHandler
+    public sealed class AddCategoryCommandHandler(
+        ICatogoriesRepository catogoriesRepository
+        ) : ICommandHandler<AddCategoryCommand, CommandResponse<CategoryDto>>
     {
+        public async Task<CommandResponse<CategoryDto>> HandleAsync(AddCategoryCommand argument)
+        {
+            var category = await catogoriesRepository.AddCategoryAsync(argument.Name, argument.Description);
+            var categoryDto = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Description = category.Description
+            };
+            return new CommandResponse<CategoryDto>(categoryDto);
+        }
     }
 }
