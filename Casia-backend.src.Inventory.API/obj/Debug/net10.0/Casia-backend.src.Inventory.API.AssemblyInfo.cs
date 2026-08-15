8ccdac00b0ee72091721c8dc336ff12856641705
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2026-08-15 17:19:09 +0200

    Prepared the skeleton of two query handlers

--- Casia-backend.src.Inventory.API/obj/Debug/net10.0/Casia-backend.src.Inventory.API.AssemblyInfo.cs
+++ Casia-backend.src.Inventory.API/obj/Debug/net10.0/Casia-backend.src.Inventory.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Casia-backend.src.Inventory.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631c9dee0cc84ffcc98c5b4380266e29cc84d723")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afa57e42e7ef0d47db8a46316846980cb83a9249")]
 [assembly: System.Reflection.AssemblyProductAttribute("Casia-backend.src.Inventory.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Casia-backend.src.Inventory.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Casia-backend.src.Inventory.Core.Application/Features/Products/Queries/GetProductById/GetProductByIdQueryHandler.cs
+++ Casia-backend.src.Inventory.Core.Application/Features/Products/Queries/GetProductById/GetProductByIdQueryHandler.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Casia_backend.src.Inventory.Core.Application.DTOs;
+using Casia_backend.src.Inventory.Core.Application.Features.Products.Queries.GetProductsById;
+using Casia_backend.src.Shared.Queries;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Casia_backend.src.Inventory.Core.Application.Features.Products.Queries.GetProductById
 {
-    public sealed class GetProductByIdQueryHandler
+    public sealed class GetProductByIdQueryHandler() : IQueryHandler<GetProductByIdQuery, QueryResponse<ProductDto>>
     {
+        public Task<QueryResponse<ProductDto>> HandleAsync(GetProductByIdQuery arguemnt)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
